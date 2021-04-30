23c565d6008c285f39b85786d7802946a5d5d9c4
Oladapo <oladapo.o@yahoo.com>
2021-04-30 06:29:32 +0100

    Second commit

--- app/src/main/java/com/builddapo/decrementfunction/MainActivity.java
+++ app/src/main/java/com/builddapo/decrementfunction/MainActivity.java
@@ -3,12 +3,31 @@ package com.builddapo.decrementfunction;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button inc;
+    TextView tV;
+    int count;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inc = findViewById(R.id.increment);
+        tV = findViewById(R.id.textView);
+
+        inc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count++;
+                tV.setText(count +" Clicks");
+            }
+        });
     }
 }
